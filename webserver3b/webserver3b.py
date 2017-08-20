edf9edaade708de101bc3fb5353d8be0a9dbaec3
hjy <hjyzjustudy@163.com>
2017-08-20 14:00:33 +0800

    web server

---
+++ webserver3b/webserver3b.py
@@ -0,0 +1,50 @@
+import os
+import socket
+import time
+
+SERVER_ADDRESS = (HOST, PORT) = '', 8888
+REQUEST_QUEUE_SIZE = 5
+
+def handle_request(client_connection):
+	request = client_connection.recv(1024)
+	print(
+		'Child PID: {pid}. Parent PID {ppid}'.format(
+			pid = os.getpid(),
+			ppid = os.getppid(),
+		)
+	)
+	print(request.decode())
+	http_response = b"""
+HTTP/1.1 200 OK
+
+Hello, World!
+"""
+	client_connection.sendall(http_response)
+	time.sleep(10)
+
+def serve_forever():
+
+	#create listen socket
+	listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	listen_socket.bind(SERVER_ADDRESS)
+	listen_socket.listen(REQUEST_QUEUE_SIZE)
+	print('Serving HTTP on port {port} ...'.format(port=PORT))
+	print('Parent PID (PPID): {pid}n'.format(pid=os.getpid()))
+
+	while True:
+
+		#turn listen socket to accetp socket
+		client_connection, client_address = listen_socket.accept()
+
+		pid = os.fork()
+		if pid == 0:
+			listen_socket.close()
+			handle_request(client_connection)
+			client_connection.close()
+			os._exit(0)
+		else:
+			client_connection.close()
+
+if __name__ == '__main__':
+	serve_forever()
